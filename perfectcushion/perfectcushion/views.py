a2d5589e7b782506df038d8ebc3075415e987e5f
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2019-03-05 13:45:27 -0600

    Fix 'text_var' but in index.html

--- perfectcushion/perfectcushion/views.py
+++ perfectcushion/perfectcushion/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
 
 class HomePage(TemplateView):
-    text_var = 'This is my fouth django app web page'
+    text_var = 'This is my fourth django app web page'
     template_name = 'index.html'
