7cd868d80896a12b2ff29f54ef83caba3cb73fbf
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2019-03-05 15:11:22 -0600

    Add Product,Category to shop.models

--- perfectcushion/perfectcushion/urls.py
+++ perfectcushion/perfectcushion/urls.py
@@ -17,8 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomePage.as_view(), name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns =+ static(settings.MEDIA_URL, document_root=settings.MEDIA_URL)
--- perfectcushion/shop/models.py
+++ perfectcushion/shop/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    imgage = models.ImageField(upload_to='category', blank=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=14, decimal_places=2)
+    image = models.ImageField(upload_to='product', blank=True)
+    stock = models.IntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'product'
+        verbose_name_plural = 'products'
+
+    def __str__(self):
+        return self.name
