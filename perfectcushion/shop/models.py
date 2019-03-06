27da303b1f73efb98b8920f12facd7ff690e04ab
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2019-03-06 11:19:56 -0600

    Add _footer.html

--- perfectcushion/shop/models.py
+++ perfectcushion/shop/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils.text import slugify
 
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=250, unique=True)
