19750a43c9d815b0f6a7827738a2e7794f2db120
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2019-03-07 12:22:19 -0600

    create shop views/url.py

--- perfectcushion/shop/views.py
+++ perfectcushion/shop/views.py
@@ -3,3 +3,14 @@ from django.http import HttpResponse
 from django.views import generic
 from . import models
 # Create your views here.
+
+
+def allProdCat(request, c_slug=None):
+    c_page= None
+    products = None
+    if c_slug = None:
+        c_page = get_object_or_404(Category, slug=c_slug)
+        products = Products.objects.filter(category=c_page, available=True)
+    else:
+        products = Product.objects.all().filter(available=True)
+    return render(request, 'shop/category.html', {'category':c_page, 'products':products})
