5fe1e75b9e91751920ddf8e41bb8a6d4597064da
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2019-03-07 16:16:47 -0600

    Fighting with meida files

--- perfectcushion/shop/views.py
+++ perfectcushion/shop/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views import generic
-from . import models
+from .models import Category, Product
 # Create your views here.
 
 
 def allProdCat(request, c_slug=None):
     c_page= None
     products = None
-    if c_slug = None:
-        c_page = get_object_or_404(Category, slug=c_slug)
-        products = Products.objects.filter(category=c_page, available=True)
+    if c_slug == None:
+        # c_page = get_object_or_404(Category, slug=c_slug)
+        products = Product.objects.filter(category=c_page, available=True)
     else:
         products = Product.objects.all().filter(available=True)
     return render(request, 'shop/category.html', {'category':c_page, 'products':products})
