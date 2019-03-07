1f48798283eee3d92b22ccd94502718481556bc7
Jeremiah Wise <jeremiah.d.wise@gmail.com>
2019-03-07 14:31:24 -0600

    Add coding to category.html

--- perfectcushion/perfectcushion/urls.py
+++ perfectcushion/perfectcushion/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from django.conf.urls import include
 from django.conf import settings
@@ -23,6 +23,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomePage.as_view(), name='home'),
+    path('shop/', include('shop.urls', namespace='shop'))
 ]
 
 if settings.DEBUG:
